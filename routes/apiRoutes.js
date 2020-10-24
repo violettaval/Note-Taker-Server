6afae9f4c4f0f97f0a95a526a7c339b30ace2d03
violettaval <violetvalencia37@gmail.com>
2020-10-23 22:58:42 -0500

    fixed saving onto side bar and setup ids

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,34 @@
 var database = require("../db/db.json");
+const fs = require("fs");
 
 module.exports = function(app) {
 
   app.get("/api/notes", function (req, res) {
-    return res.json(database);
+    res.json(database);
   });
 
   app.post("/api/notes", function (req, res) {
-    database.push(req);
+    database.push(req.body);
     console.log(database);
-    return res.json(database);
+    fs.writeFileSync("../db/db.json", JSON.stringify(database));
+    res.json(req.body);
   });
-
+  
   app.delete("/api/notes/:id", function (req, res) {
     var chosen = req.params.id;
     console.log(chosen);
     for (var i = 0; i < database.length; i++) {
-      if (chosen === database[i].titles) {
+      if (chosen === database[i].id) {
         database.splice(i, 1);
         console.log(database);
+        fs.writeFileSync(database, JSON.stringify(database));
+        return res.status(200).end();
       }
     }
-    return res.json(database);
+    res.json();  
   });
   app.post("/api/notes/clearall", function (req, res) {
     database.length = 0;
-    return res.json(database);
+    res.json();
   });
 };
--- server.js
+++ server.js
@@ -1,14 +1,41 @@
 var express = require("express");
-
 var app = express();
-
+var database = require("./db/db.json");
+const fs = require("fs");
+var index = 0; //makes id's
 var PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
+app.get("/api/notes", function (req, res) {
+  res.json(database);
+});
+
+app.post("/api/notes", function (req, res) {
+  req.body.id = index;
+  index++;
+  database.push(req.body);
+  console.log(database);
+  fs.writeFileSync("./db/db.json", JSON.stringify(database));
+  res.json(req.body);
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+  var chosen = parseInt(req.params.id);
+  console.log(chosen);
+  for (var i = 0; i < database.length; i++) {
+    if (chosen === database[i].id) {
+      database.splice(i, 1);
+      console.log(database);
+      fs.writeFileSync("./db/db.json", JSON.stringify(database));
+      return res.status(200).end();
+    }
+  }
+  res.json();  
+});
+
 require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
